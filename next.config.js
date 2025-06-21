3419e6da662a5189545dc877d9860a41685b3785
LordWuis <lordwuiserous@gmail.com>
2025-06-21 12:15:07 +0530

    corrected the file name

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['http://Thecreatorshub.in'], // if you're loading external images
+    domains: ['thecreatorshub.in'], // ✅ correct format
   },
 };
 
